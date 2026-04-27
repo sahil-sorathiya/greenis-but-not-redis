6193d987d74af42244a062dd6d52ed0bf5e02349
Sahil Patel <sahil.patel4125@gmail.com>
2026-04-28 00:02:16 +0530

    Implement SUBSCRIBE stage 4 [skip ci]

--- src/main/java/server/GreenisServer.java
+++ src/main/java/server/GreenisServer.java
@@ -207,6 +207,9 @@ public class GreenisServer {
         else if(commandName.equalsIgnoreCase("SUBSCRIBE")){
             return new SubscribeCommand().execute(context);
         }
+        else if(commandName.equalsIgnoreCase("PUBLISH")){
+            return new PublishCommand().execute(context);
+        }
         else {
             return RespWriter.writeString(new RespError("ERR unknown command " + commandName));
         }
